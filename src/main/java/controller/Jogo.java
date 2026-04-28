24dde923d9f54aa8613f2bb0f9ea463748647d7d
weryson <werysonm.taguary@gmail.com>
2026-04-28 20:13:53 -0300

    Adicionado popup de fim de jogo

--- src/main/java/controller/Jogo.java
+++ src/main/java/controller/Jogo.java
@@ -5,6 +5,7 @@ import model.*;
 import view.JogoView;
 import view.Mapa;
 import view.Janela;
+import view.MensagemFimDeJogo;
 
 import javax.swing.*;
 import java.awt.*;
@@ -15,7 +16,9 @@ public class Jogo {
     private final Tabuleiro tabuleiro;
     private final Cobrinha cobrinha;
     private Mapa mapaView;
-    private Janela janela;
+    private Janela janelaView;
+    private JogoView jogoView;
+    private Timer loopJogo;
 
     public Jogo() {
         tabuleiro = new Tabuleiro();
@@ -24,24 +27,50 @@ public class Jogo {
 
     public void iniciar(Janela janelaPrincipal ) {
         this.mapaView = new Mapa(Carregar.getArtes(), tabuleiro.getMapa(), cobrinha);
-        this.janela = janelaPrincipal;
-        janela.setConteudo(new JogoView(mapaView));
+        this.janelaView = janelaPrincipal;
+        this.jogoView = new JogoView(mapaView);
+        janelaView.setConteudo(jogoView);
 
         iniciarMovimento();
         detectarTeclado();
     }
 
     public void iniciarMovimento() {
-        Timer timer = new Timer(150, _ -> {
-            cobrinha.tentarAndar(tabuleiro.getDIMENSAO());
+        loopJogo = new Timer(150, _ -> {
+            cobrinha.tentarAndar();
+
+            if(!cobrinha.estaViva()) {
+                pararJogo();
+                return;
+            }
+
             cobrinha.tentarEngolir();
             mapaView.repaint();
         });
-        timer.start();
+        loopJogo.start();
+    }
+
+    private void pararJogo() {
+        if(loopJogo != null) {
+            loopJogo.stop();
+        }
+
+        MensagemFimDeJogo telaFim = new MensagemFimDeJogo();
+        int escolha = telaFim.mostrar(cobrinha.getRazaoMorte(), janelaView);
+
+        if (escolha == 0) {
+            // Reiniciar jogo //
+            cobrinha.reiniciarEstado();
+            iniciarMovimento();
+            tabuleiro.limparLixosMapa();
+            tabuleiro.gerarLixo();
+        } else if (escolha == 1) {
+            // voltarMenuPrincipal();
+        }
     }
 
     private void detectarTeclado() {
-        janela.addKeyListener(new KeyAdapter() {
+        janelaView.addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent e) {
                 for(Direcao d: Direcao.values()) {
@@ -53,8 +82,8 @@ public class Jogo {
             }
         });
 
-        janela.setFocusable(true);
-        janela.requestFocusInWindow();
+        janelaView.setFocusable(true);
+        janelaView.requestFocusInWindow();
     }
 
     private void tentarMudarDirecao(Direcao novaDirecao) {
@@ -64,7 +93,4 @@ public class Jogo {
             mapaView.repaint();
         }
     }
-
-
-
 }
